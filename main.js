1e32f7368a6f11a4c4acbed213a2e3cd9cbc450b
Devarsh-arch <81173191+Devarsh-arch@users.noreply.github.com>
2021-12-29 23:37:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+prediction1= "";
+prediction2= "";
+Webcam.set({
+    height: 300,
+    width:350,
+    image_format: "png",
+    png_quality: 90
+});
+camera= document.getElementById("camera");
+Webcam.attach("#camera");
+function takesnapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML= '<img id="captured_image" src="'+data_uri+'">'
+    });
+}
